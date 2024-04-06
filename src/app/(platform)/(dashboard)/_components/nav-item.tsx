a526e0d1c4afe6c56d9883fb25b7c3dfea07e937
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-04-06 03:51:35 -0300

    feat: add NavItem.Skeleton

--- src/app/(platform)/(dashboard)/_components/nav-item.tsx
+++ src/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -93,3 +94,14 @@ export function NavItem({ isExpanded, isActive, organization, onExpand }: NavIte
     </AccordionItem>
   );
 }
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
+};
