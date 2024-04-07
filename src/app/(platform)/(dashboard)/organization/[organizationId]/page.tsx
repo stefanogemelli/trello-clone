9feb2a097ea35c1b0993235c4df70fe9dacff720
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-04-07 08:14:10 -0300

    feat: add OrganizationIdPage > Form

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,20 @@
-function OrganizationIdPage() {
-  return <div>Organization page!</div>;
+import { db } from "@/lib/db";
+import Board from "./board";
+import { Form } from "./form";
+
+async function OrganizationIdPage() {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default OrganizationIdPage;
