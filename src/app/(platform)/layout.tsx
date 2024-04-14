01db89b6f05431dc623c8e0ea1bca15208c0abc3
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-04-14 09:51:31 -0300

    feat: add form-popover

--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,5 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 export default function PlatformLayout({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      {children}
+      <Toaster />
+    </ClerkProvider>
+  );
 }
