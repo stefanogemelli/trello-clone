a96d940a9089e077f5d3a9e23200892f92af9f78
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-04-16 15:04:55 -0300

    feat: add List & Card models to prisma.schema

--- src/actions/create-board/schema.ts
+++ src/actions/create-board/schema.ts
@@ -4,13 +4,13 @@ export const CreateBoard = z.object({
   title: z
     .string({
       required_error: "Title is required",
-      invalid_type_error: "Title must be a string",
+      invalid_type_error: "Title is required",
     })
     .min(3, {
       message: "Title is too short.",
     }),
   image: z.string({
     required_error: "Image is required",
-    invalid_type_error: "Image must be a string",
+    invalid_type_error: "Image is required",
   }),
 });
