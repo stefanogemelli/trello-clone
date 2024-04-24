a0e70dda064fb59267a551205fc4b49c8b393589
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-04-24 18:57:19 -0300

    feat: add card-modal

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,3 +1,4 @@
+import { useCardModal } from "@/hooks/use-card-modal";
 import { Draggable } from "@hello-pangea/dnd";
 import { Card } from "@prisma/client";
 
@@ -7,6 +8,8 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -15,6 +18,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-sm shadow-sm"
         >
           {data.title}
