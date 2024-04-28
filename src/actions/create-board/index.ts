93dbeb6c7114fe8b4672ad5ec9b777eddcb83f48
Stefano Gemelli <stefanogemelli06@gmail.com>
2024-04-27 23:54:13 -0300

    feat: create auditLogs

--- src/actions/create-board/index.ts
+++ src/actions/create-board/index.ts
@@ -6,6 +6,8 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -45,6 +47,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       error: "Failed to create.",
     };
   }
+
+  await createAuditLog({
+    entityTitle: board.title,
+    entityId: board.id,
+    entityType: ENTITY_TYPE.BOARD,
+    action: ACTION.CREATE,
+  });
 
   revalidatePath(`/board/${board.id}`);
   return { data: board };
--- src/components/modals/card-modal/index.tsx
+++ src/components/modals/card-modal/index.tsx
@@ -8,6 +8,8 @@ import { useQuery } from "@tanstack/react-query";
 import { Header } from "./header";
 import { Description } from "./description";
 import { Actions } from "./actions";
+import { AuditLog } from "@prisma/client";
+import { Activity } from "./activity";
 
 export const CardModal = () => {
   const id = useCardModal((state) => state.id);
@@ -19,6 +21,11 @@ export const CardModal = () => {
     queryFn: () => fetcher(`/api/cards/${id}`),
   });
 
+  const { data: auditLogsData } = useQuery<AuditLog[]>({
+    queryKey: ["card-logs", id],
+    queryFn: () => fetcher(`/api/cards/${id}/logs`),
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
@@ -27,6 +34,7 @@ export const CardModal = () => {
           <div className="col-span-3">
             <div className="w-full space-w-6">
               {!cardData ? <Description.Skeleton /> : <Description data={cardData} />}
+              {!auditLogsData ? <Activity.Skeleton /> : <Activity items={auditLogsData} />}
             </div>
           </div>
           {!cardData ? <Actions.Skeleton /> : <Actions data={cardData} />}
